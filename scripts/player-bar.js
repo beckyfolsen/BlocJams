d5d278d51c0dcd23900b86368bd8b69663507d91
beckyfolsen <bckolsn@gmail.com>
2019-05-21 15:51:32 -0400

    helper update

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
      $(this).attr('playState', player.playState);
      $('#time-control .total-time').text( totalTime );
   });
@@ -12,7 +12,7 @@
      const nextSongIndex = currentSongIndex + 1;
      if (nextSongIndex >= album.songs.length) { return; }
      const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+     helper.playPauseAndUpdate(nextSong);
      $('#time-control .total-time').text( nextSong.duration );
    });
 
@@ -23,7 +23,7 @@
      const nextSongIndex = currentSongIndex - 1;
      if (nextSongIndex >= album.songs.length) { return; }
      const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+     helper.playPauseAndUpdate(nextSong);
      $('#time-control .total-time').text( nextSong.duration );
    });
    
